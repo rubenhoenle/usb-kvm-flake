cb2a9d185c18bf1c4df3bc081081f88ddb543f60
Leon Schuermann <leon@is.currently.online>
2024-12-28 10:13:25 -0500

    WIP

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "USBKVM Nix Flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -10,186 +10,181 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, treefmt-nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+  outputs = { self, nixpkgs, flake-utils, treefmt-nix, }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
 
-        treefmtEval = treefmt-nix.lib.evalModule pkgs {
-          projectRootFile = "flake.nix";
-          programs.nixpkgs-fmt.enable = true;
-          programs.prettier = {
-            enable = true;
-            includes = [ "*.md" "*.yaml" "*.yml" ];
-          };
-        };
+      usbkvmVersion = "0.1.0";
 
-        repo = pkgs.fetchFromGitHub {
-          owner = "carrotIndustries";
-          repo = "usbkvm";
-          fetchSubmodules = true;
-          rev = "v0.0.19";
-          hash = "sha256-ZYS4strz85jR20kFtUpfqYhgb4p+G8VsVfJwO8sF5FM=";
-        };
-
-        fw-boot = pkgs.stdenv.mkDerivation {
-          name = "usbkvm-fw-boot";
-          src = repo;
-          nativeBuildInputs = with pkgs; [
-            python3
-            gcc-arm-embedded-9
+      treefmtEval = treefmt-nix.lib.evalModule pkgs {
+        projectRootFile = "flake.nix";
+        programs.nixpkgs-fmt.enable = true;
+        programs.prettier = {
+          enable = true;
+          includes = [
+            "*.md"
+            "*.yaml"
+            "*.yml"
           ];
-          buildPhase = ''
-            cd fw/boot
-            make
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp -r build $out
-          '';
         };
+      };
 
-        fw-usbkvm = pkgs.stdenv.mkDerivation {
-          name = "usbkvm-fw-usbkvm";
-          src = repo;
-          nativeBuildInputs = with pkgs; [
-            python3
-            gcc-arm-embedded-9
-          ];
-          patchPhase = ''
-            patchShebangs fw/usbkvm/write_header.py
-          '';
-          buildPhase = ''
-            cd fw/usbkvm
-            ls -lisa
-            make
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp -r build $out
-          '';
-        };
+      repo = pkgs.fetchFromGitHub {
+        owner = "carrotIndustries";
+        repo = "usbkvm";
+        rev = "v${usbkvmVersion}";
+        hash = "sha256-91Z2Y0O4GeCr7GVBHJ9wnMAgJ4WgxB74KgmJjrzzSNg=";
+        fetchSubmodules = true;
+      };
 
-        ms-tools-lib = pkgs.buildGoModule {
-          name = "usbkvm-ms-tools-lib";
+      fw-boot = pkgs.stdenv.mkDerivation {
+        pname = "usbkvm-fw-boot";
+        version = usbkvmVersion;
+        src = repo;
 
-          src = "${repo}/app/ms-tools";
-          vendorHash = "sha256-imHpsos7RDpATSZFWRxug67F7VgjRTT1SkLt7cWk6tU=";
+        nativeBuildInputs = with pkgs; [
+          python3
+          gcc-arm-embedded
+        ];
 
-          buildInputs = with pkgs; [
-            hidapi
-            udev
-          ];
+        buildPhase = ''
+          cd fw/boot
+          make
+        '';
 
-          prePatch = ''
+        installPhase = ''
+          mkdir -p $out
+          cp -r build $out
+        '';
+      };
+
+      fw-usbkvm = pkgs.stdenv.mkDerivation {
+        pname = "usbkvm-fw-usbkvm";
+        version = usbkvmVersion;
+
+        src = repo;
+
+        nativeBuildInputs = with pkgs; [
+          python3
+          gcc-arm-embedded
+        ];
+
+        patchPhase = ''
+          patchShebangs fw/usbkvm/write_header.py
+        '';
+
+        buildPhase = ''
+          cd fw/usbkvm
+          ls -lisa
+          make
+        '';
+
+        installPhase = ''
+          mkdir -p $out
+          cp -r build $out
+        '';
+      };
+
+      ms-tools-lib = pkgs.buildGoModule {
+        pname = "usbkvm-ms-tools-lib";
+        version = usbkvmVersion;
+
+        src = "${repo}/app/ms-tools";
+        vendorHash = "sha256-imHpsos7RDpATSZFWRxug67F7VgjRTT1SkLt7cWk6tU=";
+
+        buildInputs = with pkgs; [
+          hidapi
+          udev
+        ];
+
+        prePatch = ''
             rm lib/mslib-test.c
           '';
 
-          buildPhase = ''
+        buildPhase = ''
             mkdir -p $out/
             go build -C lib/ -o $out/ -buildmode=c-archive mslib.go
           '';
 
-          installPhase = "true";
-        };
-
-        app = pkgs.stdenv.mkDerivation {
-          name = "usbkvm-app";
-          src = repo;
-          buildInputs = with pkgs; [
-            #gstreamer
-            gst_all_1.gstreamer
-            #gtkmm 3.0
-            gtkmm3
-            #gst-plugin-gtk
-            #gst-plugins-good
-            gst_all_1.gst-plugins-good
-            hidapi
-            go
-
-            pkg-config
-            python3
-            ms-tools-lib
-          ];
-
-          nativeBuildInputs = with pkgs; [
-            meson
-            ninja
-          ];
-
-          prePatch = ''
-            cp -rf ${fw-boot}/build fw/boot
-            cp -rf ${fw-usbkvm}/build fw/usbkvm
-          '';
-
-          patches = [
-            ./0001-app-meson.build-use-mslib-artifacts-precompiled-by-N.patch
-          ];
-
-          postPatch = ''
-            ${pkgs.envsubst}/bin/envsubst -i app/meson.build -o app/meson.build.patched
-            mv app/meson.build.patched app/meson.build
-            cd app/
-          '';
-
-          MSLIB_A_PRECOMPILED = "${ms-tools-lib}/mslib.a";
-          MSLIB_H_PRECOMPILED = "${ms-tools-lib}/mslib.h";
-
-          # buildPhase = ''
-          #   # ls -lisa .
-          #   # cd app
-          #   # mkdir build
-          #   ${pkgs.meson}/bin/meson setup build
-          #   #${pkgs.meson}/bin/meson compile -C build
-          # '';
-          # installPhase = ''
-          #   mkdir -p $out/bin
-          #   #cp app/build/usbkvm $out/bin
-          # '';
-        };
-
-        app-from-tarball = pkgs.stdenv.mkDerivation {
-          name = "usbkvm-app-from-tarball";
-          src = builtins.fetchurl {
-        url = "https://github.com/carrotIndustries/usbkvm/releases/download/v0.1.0/usbkvm-v0.1.0.tar.gz";
-        sha256 = "01yzhqbrcxah08paxw1flmhw6igab3qwzly4yri6d45xq3vfw6mc";
+        installPhase = "true";
       };
-          buildInputs = with pkgs; [
-            #gstreamer
-            gst_all_1.gstreamer
-            #gtkmm 3.0
-            gtkmm3
-            #gst-plugin-gtk
-            #gst-plugins-good
-            gst_all_1.gst-plugins-good
-            hidapi
-            go
 
-            pkg-config
-            python3
+      app = pkgs.stdenv.mkDerivation rec {
+        pname = "usbkvm-app";
+        version = usbkvmVersion;
+        src = repo;
 
-            gst_all_1.gstreamer gtkmm3 meson hidapi go gst_all_1.gst-plugins-good gtk3 cmake pkg-config qemu udev ninja
-          ];
-          nativeBuildInputs = with pkgs; [
-            cmake
-          ];
-          dontUseCmakeConfigure = true;
-          buildPhase = ''
-            ls -lisa .
+        buildInputs = with pkgs; [
+          gst_all_1.gstreamer
+          gtkmm3
+          hidapi
+        ];
 
-            #cd app
-            mkdir build
-            ${pkgs.meson}/bin/meson build
-            #${pkgs.meson}/bin/meson compile -C build
+        nativeBuildInputs = with pkgs; [
+          go
+          pkg-config
+          python3
+          meson
+          ninja
+          makeWrapper
+        ];
+
+        prePatch = ''
+          cp -rf ${fw-boot}/build fw/boot
+          cp -rf ${fw-usbkvm}/build fw/usbkvm
+        '';
+
+        patches = [
+          ./0001-app-meson.build-use-mslib-artifacts-precompiled-by-N.patch
+        ];
+
+        MSLIB_A_PRECOMPILED = "${ms-tools-lib}/mslib.a";
+        MSLIB_H_PRECOMPILED = "${ms-tools-lib}/mslib.h";
+
+        postPatch = ''
+          ${pkgs.envsubst}/bin/envsubst -i app/meson.build -o app/meson.build.patched
+          mv app/meson.build.patched app/meson.build
+          cd app/
+        '';
+
+        postFixup = let
+          GST_PLUGIN_PATH = pkgs.lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (
+            with pkgs;
+            [
+              gst_all_1.gst-plugins-base
+              (gst_all_1.gst-plugins-good.override { gtkSupport = true; })
+            ]
+          );
+        in
+          pkgs.lib.optionalString pkgs.stdenv.hostPlatform.isLinux ''
+            wrapProgram $out/bin/usbkvm --prefix GST_PLUGIN_PATH : ${GST_PLUGIN_PATH}
           '';
-          installPhase = ''
-            mkdir -p $out/bin
-            #cp app/build/usbkvm $out/bin
-          '';
+
+        postInstall = ''
+          mkdir -p $out/lib/udev/rules.d/
+          cp $src/app/70-usbkvm.rules $out/lib/udev/rules.d/
+        '';
+      };
+
+      app-from-tarball = pkgs.stdenv.mkDerivation {
+        name = "usbkvm-app-from-tarball";
+        src = builtins.fetchurl {
+          url = "https://github.com/carrotIndustries/usbkvm/releases/download/v${usbkvmVersion}/usbkvm-v${usbkvmVersion}.tar.gz";
+          sha256 = "01yzhqbrcxah08paxw1flmhw6igab3qwzly4yri6d45xq3vfw6mc";
         };
-      in
+
+        postPatch = ''
+          cd app/
+        '';
+
+        inherit (app)
+          buildInputs
+          nativeBuildInputs
+          postFixup;
+      };
+    in
       {
         formatter = treefmtEval.config.build.wrapper;
         checks.formatter = treefmtEval.config.build.check self;
@@ -199,6 +194,26 @@
           fw-boot = fw-boot;
           fw-usbkvm = fw-usbkvm;
           tarball = app-from-tarball;
+        };
+
+        devShells.default = pkgs.mkShell {
+          name = "usbkvm";
+
+          packages = with pkgs; [
+            pkg-config
+            gcc
+            gdb
+            meson
+            ninja
+            gst_all_1.gstreamer
+            go
+            gtkmm3
+            hidapi
+            gst_all_1.gst-plugins-good
+            (gst_all_1.gst-plugins-good.override { gtkSupport = true; })
+            gst_all_1.gst-plugins-base
+            udev
+          ];
         };
       }
     );
